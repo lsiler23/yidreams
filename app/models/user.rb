1af0ffd159db9a703080ca092f8c25f46aba8629
Laurette <siler.laurette@gmail.com>
2019-05-26 17:08:41 -0400

    user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,35 @@
-class CreateUser < ApplicationRecord
+class User < ApplicationRecord
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_creds(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
